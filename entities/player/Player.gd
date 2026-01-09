968d3711c2d82dd239670f29685dfdbf05163742
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-09 17:51:08 -0300

    LG-2 better structure Changed signal name to 'walk' Removed speed variable, added movement_data resource with speed, acceleration and friction inside Changed _physics_process to _process to handle input correctly, faster Removed normalization logic from entity, wrote it in the system

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 class_name Player
 
-@export var speed : float = 100.0
-signal move_requested(direction: Vector2)
+@export var movement_data : MovementResource
+signal walk(direction: Vector2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void: 
-	var direction := Vector2(
+func _process(delta: float) -> void: 
+	var raw_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized()
+	)
 	
-	move_requested.emit(direction)
+	walk.emit(raw_direction)
