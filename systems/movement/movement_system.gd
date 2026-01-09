f9a71da5e35ba59ee906a7f8ef60246dce1e4e15
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-09 17:48:48 -0300

    LG-2 better structure Added parameters for speed, acc, fric, dir and vel to System with default values Changed body type to CharacterBody2D to make this system reusable with different entities Added error checks in the ready function to ensure that the entity associated with the system is of type CharacterBody2D and/or extends it Added error check for when the entity hasnt movement_data Added assigns for speed, acceleration and friction given a movement_data resource Added error check for when the entity doesnt have the 'walk' signal Changed signal name from movement_requested to walk Removed logic from signal function, now it only changes direction when called Changed logic in physics fun to handle all movement, acceleration and friction included

--- systems/movement/movement_system.gd
+++ systems/movement/movement_system.gd
@@ -1,32 +1,55 @@
 extends Node
 class_name MovementSystem
 
-@export var acceleration : float = 1200.0
-@export var friction : float = 800.0
-
-var _body: Player
-var _speed: float
+var _body: CharacterBody2D
+var speed : float = 120
+var acceleration : float = 1200
+var friction: float = 1200
 var direction := Vector2.ZERO
+var velocity := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_body = get_parent() as Player
-	_speed = _body.speed
+	# Obtains the movement system associated node's entity object which extends 
+	# from (or is) CharacterBody2D
+	var specific_entity = get_parent()
 	
-	if _body.has_signal("move_requested"):
-		_body.move_requested.connect(_on_move_requested)
+	# Ensure we are working with a CharacterBody2D
+	if specific_entity is CharacterBody2D:
+		_body = specific_entity
+	elif specific_entity.get_parent() is CharacterBody2D:
+		_body = specific_entity.get_parent()
+	else:
+		push_error("MovementSystem's associated Node must be child of CharacterBody2D")
+		return
 
-func _on_move_requested(direction: Vector2):
-	var target_velocity := direction * _speed
-	_body.velocity = _body.velocity.move_toward(
-		target_velocity,
-		acceleration * get_physics_process_delta_time()
-	)
+	# Get movement data if present
+	if "movement_data" in specific_entity:
+		speed = specific_entity.movement_data.speed
+		acceleration = specific_entity.movement_data.acceleration
+		friction = specific_entity.movement_data.friction
+	else:
+		push_error("Entity has no movement_data, using system's defaults")
+		
+	if _body.has_signal("walk"):
+		_body.walk.connect(_on_walk_requested)
+	else:
+		push_error("Entity has no move_requested signal")
+
+func _on_walk_requested(dir: Vector2):
+	direction = dir.normalized()
 
 func _physics_process(delta: float) -> void:
-	if _body.velocity.length() > 0.0:
-		_body.velocity = _body.velocity.move_toward(
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(
+			direction * speed,
+			acceleration * delta
+		)
+	else:
+		velocity = velocity.move_toward(
 			Vector2.ZERO,
 			friction * delta
 		)
+
+	_body.velocity = velocity
 	_body.move_and_slide()
