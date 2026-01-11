7aca580b95544ee08ce08b972a03705c615e30b3
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-11 12:30:15 -0300

    LG-2 added dash data

--- data/MovementResource.gd
+++ data/MovementResource.gd
@@ -4,3 +4,6 @@ class_name MovementResource
 @export var speed : float
 @export var acceleration : float
 @export var friction : float
+@export var dash_speed: float
+@export var dash_duration: float
+@export var dash_cooldown: float
