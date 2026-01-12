dac5bb5c25167a5516d19b7c3355a056687bf321
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-12 02:18:51 -0300

    LG-2 movement fsm and states for different moves implemented

---
+++ systems/fsm/state.gd
@@ -0,0 +1,8 @@
+extends Node
+class_name State
+
+var system
+
+func enter(): pass
+func exit(): pass
+func update(_delta): pass
---
+++ systems/movement/states/movement_state.gd
@@ -0,0 +1,8 @@
+extends State
+class_name MovementState
+
+var movement  # referencia al MovementSystem
+
+func enter(): pass
+func exit(): pass
+func update(_delta): pass
