c2a8c5b75cd8cefb7248995ad3bb5dc7ff9b7e0a
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-12 02:20:04 -0300

    LG-2 demo for movement system created

---
+++ demos/movement_system/PlayerDemo.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+class_name PlayerDemo
+
+@export var movement_data : MovementResource
+signal walk(direction: Vector2)
+signal dash(dash: bool)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(_delta: float) -> void: 
+	var raw_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	walk.emit(raw_direction)
+	
+	var dashed = Input.get_action_strength("dash")
+	if dashed:
+		dash.emit()
