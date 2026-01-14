918bb82769db2ffb49a1ff4af225de6df8306daa
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-13 22:07:04 -0300

    LG-4 added prev_state and state_changed signal

--- systems/fsm/fsm.gd
+++ systems/fsm/fsm.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name FSM
 
+signal state_changed(new_state: State)
+
+var prev_state: State
 var current_state: State
 
 func change_state(new_state: State):
@@ -11,8 +14,10 @@ func change_state(new_state: State):
 		
 	if current_state:
 		current_state.exit()
-		
+	
+	prev_state = current_state
 	current_state = new_state
+	state_changed.emit(current_state)
 	print("[FSM] → ", current_state.name)
 	current_state.enter()
 	
