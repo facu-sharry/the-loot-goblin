5d30bdc65ebc4a8adef7b3f08a44d978bae65d1d
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-13 22:06:40 -0300

    LG-4 initial state emition

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -7,7 +7,7 @@ signal dash(dash: bool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	walk.emit(Vector2.ZERO)
 
 func _process(_delta: float) -> void: 
 	var raw_direction = Vector2(
