8371e8bf2a3809297caaa20d41cf49c4c5c42b6f
Facundo Sharry <facundo.sharry@gmail.com>
2026-06-23 18:02:29 -0300

    LG-6 added AttackResource base class

---
+++ data/AttackResource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name AttackResource
+
+@export var attack_damage : float
+@export var attack_speed: float
+@export var attack_duration: float
+@export var attack_cooldown: float
+@export var attack_weight: float
