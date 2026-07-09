e5d64f54fc182123b1583836dee8042b792a7c6b
Facundo Sharry <facundo.sharry@gmail.com>
2026-07-09 19:05:05 -0300

    LG-6 added cooldown and time_left logic to attack1 state also had to import movement.idle_state to this state so it can be used

--- systems/attack/states/attack1_state.gd
+++ systems/attack/states/attack1_state.gd
@@ -1,8 +1,25 @@
 extends AttackState
 class_name Attack1State
 
+var time_left := 0.0
+var cooldown_left := 0.0
+
 func _init(p_name: String):
 	name = p_name
 
+func can_enter() -> bool:
+	return cooldown_left <= 0.0
+
+func enter():
+	time_left = attack.data.attack_duration
+
 func update(delta):
-	pass
+	if time_left > 0.0:
+		time_left -= delta
+		if time_left <= 0:
+			cooldown_left = attack.data.attack_cooldown
+			attack.fsm.change_state(movement.idle_state)
+
+func tick(delta):
+	if cooldown_left > 0.0:
+		cooldown_left -= delta
