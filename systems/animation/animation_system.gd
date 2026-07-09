8ed341a8fb89c6dc17b3b29d4ed2bf63995fe2a7
Facundo Sharry <facundo.sharry@gmail.com>
2026-07-09 19:02:12 -0300

    LG-6 merged animation logic to a single function given a single fsm moved initialization logic to a function that waits for all the systems to initialize

--- systems/animation/animation_system.gd
+++ systems/animation/animation_system.gd
@@ -4,13 +4,27 @@ class_name AnimationSystem
 @onready var animator : AnimationPlayer = $"../AnimationPlayer"
 @onready var sprite : Sprite2D = $"../Sprite2D"
 
+var _body: CharacterBody2D
+
 func _ready():
-	var movement = get_parent().get_node("MovementSystem")
-	movement.movement_fsm.state_changed.connect(_on_state_changed)
-	movement.facing_changed.connect(_on_facing_changed)
+	# Obtains the movement system associated node's entity object which extends 
+	# from (or is) CharacterBody2D
+	var specific_entity = get_parent()
 	
-	var attack = get_parent().get_node("AttackSystem")
-	attack.attack_fsm.state_changed.connect(_on_state_attack_changed)
+	# Ensure we are working with a CharacterBody2D
+	if specific_entity is CharacterBody2D:
+		_body = specific_entity
+		specific_entity.systems_ready.connect(_on_systems_ready)
+	else:
+		push_error("MovementSystem's associated Node must be child of CharacterBody2D")
+		return
+
+func _on_systems_ready(_movement: MovementSystem, _attack: AttackSystem, _anim: AnimationSystem):
+	# Connects the state_changed signal from the FSM to the animation system
+	_body.fsm.state_changed.connect(_on_state_changed)
+
+	var movement = _body.get_node("MovementSystem")
+	movement.facing_changed.connect(_on_facing_changed)
 	
 func _on_state_changed(state: State):
 	if not animator:
@@ -24,15 +38,8 @@ func _on_state_changed(state: State):
 			animator.play("walk")
 		"Dash":
 			animator.play("dash")
+		"Attack1":
+			animator.play("attack1")
 
 func _on_facing_changed(dir: Vector2):
 	sprite.flip_h = dir.x < 0
-	
-func _on_state_attack_changed(state: State):
-	if not animator:
-		push_error("Animation Player not found")
-		return
-	
-	match state.name:
-		"Attack1":
-			animator.play("attack1")
