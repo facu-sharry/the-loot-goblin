4d3004f7e0a4892dfe62986d809fd53473ba1ba7
Facundo Sharry <facundo.sharry@gmail.com>
2026-07-09 19:04:12 -0300

    LG-6 implemented logic to wait for all the systems to be initialized before using the fsm removed logic for initial state in attack system, it only handles attack1

--- systems/attack/attack_system.gd
+++ systems/attack/attack_system.gd
@@ -3,10 +3,12 @@ class_name AttackSystem
 
 # signal facing_changed(dir: Vector2)
 
+var fsm: FSM
+var _systems_initialized: bool = false
+
 # State Machines for Attacks
-var attack_fsm: AttackFSM
 var attack1_state: Attack1State
-var idle_state: IdleAttState
+var idle_state: IdleState
 # State Machines for Attacks - end
 
 # Attack System parameters
@@ -18,19 +20,6 @@ var _body: CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	attack_fsm = AttackFSM.new(self)
-	add_child(attack_fsm)
-	
-	attack1_state = Attack1State.new("Attack1")
-	idle_state = IdleAttState.new("Idle");
-	
-	for state in [idle_state,attack1_state]:
-		state.attack = self
-		attack_fsm.add_child(state)
-		
-	attack_fsm.change_state(idle_state)
-	call_deferred("_emit_initial_state")
-	
 	# Obtains the system associated node's entity object which extends 
 	# from (or is) CharacterBody2D
 	var specific_entity = get_parent()
@@ -38,6 +27,7 @@ func _ready() -> void:
 	# Ensure we are working with a CharacterBody2D
 	if specific_entity is CharacterBody2D:
 		_body = specific_entity
+		specific_entity.systems_ready.connect(_on_systems_ready)
 	else:
 		push_error("System's associated Node must be child of CharacterBody2D")
 		return
@@ -52,18 +42,33 @@ func _ready() -> void:
 		_body.attack.connect(_on_attack_requested)
 	else:
 		push_error("Entity has no attack signal")
-		
-	pass
+
+func _on_systems_ready(_movement: MovementSystem, _attack: AttackSystem, _anim: AnimationSystem):
+	fsm = _body.fsm
+	# Get the FSM from the entity if present
+	if "fsm" in _body and _body.fsm != null:
+		fsm = _body.fsm
+	else:
+		push_error("Entity has no FSM configured or associated, system failure")
+
+	attack1_state = Attack1State.new("Attack1")
+	
+	for state in [attack1_state]:
+		state.attack = self
+		state.movement = _movement
+		state.fsm = fsm
+		fsm.add_child(state)
+
+	_systems_initialized = true
 
 func _physics_process(delta: float) -> void:
-	attack_fsm.update(delta)
-	
-	pass
-	
-func _emit_initial_state():
-	attack_fsm.state_changed.emit(attack_fsm.current_state)
+	if not _systems_initialized or fsm == null:
+		return
+
+	fsm.update(delta)
 	
 
 func _on_attack_requested():
-	attack_fsm.state_changed.emit(attack1_state)
+	if _systems_initialized and fsm:
+		fsm.change_state(attack1_state)
 			
