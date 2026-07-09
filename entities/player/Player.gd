b7df1b43704691632c5347b4d24c8f5abb0890ce
Facundo Sharry <facundo.sharry@gmail.com>
2026-07-09 19:01:00 -0300

    LG-6 Adjusted some animation frames Initialized central FSM in player script

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,21 +1,45 @@
 extends CharacterBody2D
 class_name Player
 
-@export var movement_data : MovementResource
+@export var movement_data: MovementResource
 signal walk(direction: Vector2)
 signal dash(dash: bool)
 
-@export var attack_data : AttackResource
+@export var attack_data: AttackResource
 signal attack()
 
-# Called when the node enters the scene tree for the first time.
+signal systems_ready(movement: MovementSystem, attack: AttackSystem, anim: AnimationSystem)
+
+var fsm: FSM
+var movement_system: MovementSystem
+var attack_system: AttackSystem
+var animation_system: AnimationSystem
+
 func _ready() -> void:
+	# PASO 1: Crear la FSM
+	fsm = FSM.new()
+	add_child(fsm)
+	
+	# PASO 2: Obtener referencias a los sistemas (ya existen en el árbol)
+	movement_system = $MovementSystem
+	attack_system = $AttackSystem
+	animation_system = $AnimationSystem
+	
+	# PASO 3: Esperar a que los _ready() de los sistemas terminen
+	#         Luego emitir la señal
+	call_deferred("_on_all_systems_ready")
+
+func _on_all_systems_ready() -> void:
+	# PASO 4: Ahora los sistemas pueden usar fsm, movement_data, etc.
+	systems_ready.emit(movement_system, attack_system, animation_system)
+	
+	# PASO 5: Input inicial
 	walk.emit(Vector2.ZERO)
 
 func _process(_delta: float) -> void: 
 	var raw_direction = Vector2(
-		Input.get_axis("left","right"),
-		Input.get_axis("up","down")
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
 	)
 	
 	walk.emit(raw_direction)
